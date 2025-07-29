8a0f395a8136fe4562fbf250dda0da34370d780f
Grimkel <opopopa705@gmail.com>
2025-07-29 16:06:07 +0300

    add front

---
+++ webapp/classifier/classifier.py
@@ -0,0 +1,39 @@
+import pickle
+
+import numpy as np
+
+from util.preprocess import preprocess, target_names
+
+class SVCmodel:
+
+    # Инициализация модели по пути с .pickle файлами - path
+    def __init__(self, path: str):
+        # путь к эмбеддингу
+        vecPath = path + "/vec.pickle"
+        # путь к модели
+        svcPath = path + "/svc.pickle"
+
+        # Загрузка tf-idf эмбеддинга
+        self.vectorizer = None
+        with open(vecPath, 'rb') as f:
+            self.vectorizer = pickle.load(f)
+
+        # Загрузка SVC модели
+        self.svc = None
+        with open(svcPath, 'rb') as f:
+            self.svc = pickle.load(f)
+
+    # Классификация переданного текста
+    def transform(self, text: str) -> str:
+        # Препроцессин и веторизация
+        processedText = preprocess(text)
+        vectorized = self.vectorizer.transform([processedText])
+
+        # Классификация - вывод npмассив
+        out = self.svc.predict(vectorized)
+        # Выделение числа - ID класса из массива np.array -> int
+        clsId = np.squeeze(out)
+        # Получить название класса
+        cls = target_names[clsId]
+
+        return cls
--- webapp/main.py
+++ webapp/main.py
@@ -1,11 +1,54 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+
+from util.preprocess import getText
+
+from classifier.classifier import SVCmodel
 
 app = Flask(__name__)
 
+svc = SVCmodel("./classifier/SVC")
+
+# Принятие файла с фронтенда и ответ классом документа
 @app.route("/predict",  methods=['POST'])
 def predict():
-    pass
 
+    # Проверка что файл был передан
+    if 'file' not in request.files:
+        return 'No file provided'
+    
+    # Ответ сервера
+    output = "-"
+
+    try:
+        # Сохранение файла
+        file = request.files['file']
+        file.save("temp.pdf")
+
+        # Является ли файл валидным pdf
+        valid = True
+        text = None
+        try:
+            text = getText("temp.pdf")
+        except:
+            valid = False
+            output = "invalid file"
+
+        # Если файл валидный то он классифицируется
+        if valid:
+            output = svc.transform(text)
+
+    # Обработка иных ошибок
+    except:
+        output = "unexpected error"
+    # Удаление файла в случае непредвиденной ошибки
+    finally:
+        os.remove("temp.pdf")
+
+    return output
+
+# Основная страница с формой для классификации документов
 @app.route("/")
 def mainpage():
     return render_template("index.html")
